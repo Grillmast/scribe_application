102e6691c5e34ba4e4850452db0a331eee49415e
Cody <bcuy46@yahoo.com>
2023-04-02 20:15:21 -0400

    Updated majority of back-end code. Added routes folder and included a new file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require('path');
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('assets'));
+
+
+
+app.listen(PORT, () => 
+    console.log(`Server listening at http://localhost:${PORT}`)
+);
